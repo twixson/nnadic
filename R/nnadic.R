c970a3017cc3014786eb1033eb4cbcbfd8a3b8c1
Troy P. Wixson <troy.wixson12@gmail.com>
2023-09-28 15:29:39 -0600

    fixed nnadic() to include one_test to distinguish between assessing nnadic and using nnadic to determine if a single dataset is AD/AI.

--- R/nnadic.R
+++ R/nnadic.R
@@ -2,24 +2,33 @@
 #'
 #' @param data a matrix of dimension \eqn{d}x\eqn{2}.
 #'  Currently only supports \eqn{d=500}
+#'@param one_test change to `FALSE` if there are multiple datasets in your test
+#'  This can be used to test nnadic on simulated datasets where the truth is
+#'  known or on multiple unknown datasets at the same time.
+#'@param make_hist change to `FALSE` if you do not want a histogram of the
+#'  probabilities to be returned.
 #'
-#' @return a `list` with two items:
+#' @return a `list` with two or three items:
 #'  1) probs - a vector of probabilities of being AI
 #'  2) preds - a vector of predictions (0 is AD, 1 is AI)
+#'  3) mean  - the mean of the predictions, included when one_test is TRUE
 #' @export
 #'
 #' @examples
 #' nnadic(matrix(rnorm(1000), ncol = 2))
-nnadic <- function(data, make_hist = TRUE){
+nnadic <- function(data, one_test = TRUE, make_hist = TRUE){
   results <- list()
   results$probs <- stats::predict(object = model, data)
   results$preds <- ifelse(probabilities >= 0.5, 1, 0)
-  results$mean  <- mean(preds)
+  if(one_list){
+    results$mean  <- mean(preds)
+    print(paste0("The mean of the predictions is: ", results$mean,
+                 " which is `nnadic`'s probability that these data are AI"))
+  }
   if(make_hist){
     hist(results$probs)
   }
-  print(paste0("The mean of the predictions is: ", results$mean,
-               " which is `nnadic`'s probability that these data are AI"))
-  print("Probabilities and predictions for each dataset have been returned")
+  print("Probabilities and predictions for each dataset have been returned,
+        each probability is the probability of AI which is coded as '1'")
   return(results)
 }
