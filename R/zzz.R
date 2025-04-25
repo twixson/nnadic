455b7ea69a46c8967302e3e15fc1735f47703756
twixson <58630522+twixson@users.noreply.github.com>
2025-04-24 18:48:55 -0600

    Revert "update nnadic architecture based on revision"
    This reverts commit bdfbbf493f46aca9e4472b7ce1770d370fc59200.

--- R/zzz.R
+++ R/zzz.R
@@ -1,37 +1,56 @@
 .onLoad <- function(libname, pkgname) {
-  keras::layer_conv_1d(model, kernel_size = c(1), filters = 128, strides = 1,
-                     input_shape = c(1000, 2),
-                     bias_initializer = 
-                       keras::initializer_constant(value = 0.01))
-keras::layer_activation_leaky_relu(model)
-keras::layer_conv_1d(model, kernel_size = c(1), filters = 64,
-                     bias_initializer = 
-                       keras::initializer_constant(value = 0.01))
-keras::layer_activation_leaky_relu(model)
-keras::layer_conv_1d(model, kernel_size = c(1), filters = 32,
-                     bias_initializer = 
-                       keras::initializer_constant(value = 0.01))
-keras::layer_activation_leaky_relu(model, name = "feature_layer")
-keras::layer_average_pooling_1d(model, pool_size = 1000)
-keras::layer_flatten(model)
-keras::layer_dense(model, units = 128,
-                   kernel_initializer = keras::initializer_he_uniform(),
-                   bias_initializer = keras::initializer_constant(value = 0.01))
-keras::layer_activation_leaky_relu(model)
-keras::layer_dropout(model, 0.5)
-keras::layer_dense(model, units = 64,
-                   kernel_initializer = keras::initializer_he_uniform(),
-                   bias_initializer = keras::initializer_constant(value = 0.01))
-keras::layer_activation_leaky_relu(model)
-keras::layer_dropout(model, 0.5)
-keras::layer_dense(model, units = 32,
-                   kernel_initializer = keras::initializer_he_uniform(),
-                   bias_initializer = keras::initializer_constant(value = 0.01))
-keras::layer_activation_leaky_relu(model)
-keras::layer_dense(model, units = 1, activation = "sigmoid")
+  model <- keras::keras_model_sequential()
+
+  keras::layer_conv_1d(model, kernel_size = c(1), filters = 32, strides = 1,
+                       input_shape = c(500, 2),
+                       bias_initializer = keras::initializer_constant(value = 0.01))
+  keras::layer_activation_leaky_relu(model)
+  keras::layer_conv_1d(model, kernel_size = c(1), filters = 32,
+                       bias_initializer = keras::initializer_constant(value = 0.01))
+  keras::layer_activation_leaky_relu(model)
+  keras::layer_max_pooling_1d(model, pool_size = c(2))
+  keras::layer_conv_1d(model, kernel_size = c(1), filters = 16,
+                       bias_initializer = keras::initializer_constant(value = 0.01))
+  keras::layer_activation_leaky_relu(model)
+  keras::layer_max_pooling_1d(model, pool_size = c(2))
+  keras::layer_conv_1d(model, kernel_size = c(1), filters = 8,
+                       bias_initializer = keras::initializer_constant(value = 0.01))
+  keras::layer_activation_leaky_relu(model)
+  keras::layer_max_pooling_1d(model, pool_size = c(2))
+  keras::layer_flatten(model)
+  keras::layer_dense(model, units = 32,
+                     kernel_initializer = keras::initializer_he_uniform(),
+                     bias_initializer = keras::initializer_constant(value = 0.01))
+  keras::layer_activation_leaky_relu(model)
+  keras::layer_dropout(model, 0.5)
+  keras::layer_dense(model, units = 32,
+                     kernel_initializer = keras::initializer_he_uniform(),
+                     bias_initializer = keras::initializer_constant(value = 0.01))
+  keras::layer_activation_leaky_relu(model)
+  keras::layer_dropout(model, 0.5)
+  keras::layer_dense(model, units = 32,
+                     kernel_initializer = keras::initializer_he_uniform(),
+                     bias_initializer = keras::initializer_constant(value = 0.01))
+  keras::layer_activation_leaky_relu(model)
+  keras::layer_dropout(model, 0.5)
+  keras::layer_dense(model, units = 16,
+                     kernel_initializer = keras::initializer_he_uniform(),
+                     bias_initializer = keras::initializer_constant(value = 0.01))
+  keras::layer_activation_leaky_relu(model)
+  keras::layer_dropout(model, 0.5)
+  keras::layer_dense(model, units = 16,
+                     kernel_initializer = keras::initializer_he_uniform(),
+                     bias_initializer = keras::initializer_constant(value = 0.01))
+  keras::layer_activation_leaky_relu(model)
+  keras::layer_dropout(model, 0.5)
+  keras::layer_dense(model, units = 8,
+                     kernel_initializer = keras::initializer_he_uniform(),
+                     bias_initializer = keras::initializer_constant(value = 0.01))
+  keras::layer_activation_leaky_relu(model)
+  keras::layer_dense(model, units = 1, activation = "sigmoid")
 
   keras::load_model_weights_hdf5(model, filepath =
-      system.file("pretrained_models/final_weights_revision.hdf5",
+      system.file("pretrained_models/final_model_weights.hdf5",
                     package = "nnadic"))
     assign("model", model, envir = parent.env(environment()))
 }
